fff2458573eee2ac2f46ae1145b9a74461918400
Clément XaT <xatrixblue@gmail.com>
2024-05-02 23:04:14 +0200

    Adapt error.js for Chrome

--- rehost/errors.js
+++ rehost/errors.js
@@ -1,13 +1,13 @@
+// errors.js
+
 const g_debug = false; // TODO
 
 function error_message(p_file, p_action, p_error) {
-  console.log("ERROR extension " + browser.i18n.getMessage("extension_name") +
-    " : " + p_file + " : " + p_action + " :", p_error);
+  console.error(`ERROR extension ${chrome.i18n.getMessage("extension_name")} : ${p_file} : ${p_action} :`, p_error);
 }
 
 function debug_message(p_file, p_action, p_message) {
-  if(g_debug) {
-    console.log("DEBUG extension " + browser.i18n.getMessage("extension_name") +
-      " : " + p_file + " : " + p_action + " :", p_message);
+  if (g_debug) {
+    console.debug(`DEBUG extension ${chrome.i18n.getMessage("extension_name")} : ${p_file} : ${p_action} :`, p_message);
   }
 }
